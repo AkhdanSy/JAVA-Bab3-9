f982e54afa5c200f4465ab4f3042283da22acec0
AkhdanSy <akhdansyauqi62@gmail.com>
2025-08-19 09:04:57 +0700

    Add files via upload

---
+++ Bab5Java.java
@@ -0,0 +1,133 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab5java;
+
+/**
+ *
+ * @author Axioo
+ */
+public class Bab5Java {
+
+    public static void main(String[] args) {
+                //OPERATOR BILANGAN BULAT UNARY
+            //INCREMENT
+        //Integer sebelum = 20;
+        //Integer sesudah = 0;
+        
+        //System.out.println("Sebelum increment : "+ sebelum);
+        //sesudah=++sebelum;
+        //System.out.println("sesudah increment : "+ sesudah);
+        
+            //DECREMENT
+        //Integer sebelum = 9;
+        //Integer sesudah = 0;
+        
+        //System.out.println("Sebelum increment : "+ sebelum);
+        //sesudah=--sebelum;
+        //System.out.println("sesudah increment : "+ sesudah);
+        
+            //NEGASI
+        //int a = 2;
+        //int b = 0;
+        
+        //System.out.println("Sebelum increment : "+ a);
+        //b=-a;
+        //System.out.println("sesudah increment : "+ b);
+        
+                //OPERATOR BILANGAN BULAT BINARY
+            //PENAMBAHAN
+        //int a1 =10;
+        //int a2 =2;
+        //System.out.println("Penambahan 10 + 2 : "+ (a1+a2));
+        
+            //PENGURANGAN
+        //int b1 =10;
+        //int b2 =2;
+        //System.out.println("Pengurangan 10 - 2 :"+ (b1-b2));
+            
+            //PERKALIAN
+        //int c1=16;
+        //int c2=2;
+        //System.out.println("Perkalian 16 * 2 :"+ (c1*c2));
+            
+            //PEMBAGIAN
+        //int d1 =16;
+        //int d2 =2;
+        //System.out.println("Pembagian 16 / 2 :"+ (d1/d2));
+            
+            //SISA BAGI
+        //int e1 =16;
+        //int e2 =2;
+        //System.out.println("Sisa bagi 16 % 2 :"+ (e1%e2));
+        
+                //OPERATOR BILANGAN BULAT RELASIONAL
+            //OPERATOR KURANG DARI
+        //int a = 10;
+        //int b = 15;
+        
+        //System.out.println("Variabel a : " + a);
+        //System.out.println("Variabel b : " + b);
+        //System.out.println("Apakah 10 kurang dari 15 " + (a < b));
+        //System.out.println("Apakah 10 kurang dari 15 " + (b < a));
+        //System.out.println("Apakah 10 kurang dari 10 " + (b < b));
+        
+            //OPERATOR LEBIH DARI
+        //int a = 2;
+        //int b = 3;
+        
+        //System.out.println("Variabel a : " + a);
+        //System.out.println("Variabel b : " + b);
+        //System.out.println("Apakah 2 lebih besar dari 3 " + (a > b));
+        //System.out.println("Apakah 3 lebih besar dari 2 " + (b < a));
+        //System.out.println("Apakah 3 lebih besar dari 3 " + (b < b));
+        
+            //OPERATOR LEBIH KECIL ATAU SAMA DENGAN
+        //int a = 3;
+        //int b = 3;
+        
+        //System.out.println("Variabel a : " + a);
+        //System.out.println("Variabel b : " + b);
+        //System.out.println("Apakah 3 lebih kecil atau sama dengan 3 " + (a <= b));
+        //a = 9;
+        //System.out.println("Variabel a diubah : " + a);
+        //System.out.println("Apakah 9 lebih kecil atau sama dengan 3 " + (a <= b));
+        
+            //OPERATOR LEBIH BESAR ATAU SAMA DENGAN
+        //int a = 3;
+        //int b = 3;
+        
+        //System.out.println("Variabel a : " + a);
+        //System.out.println("Variabel b : " + b);
+        //System.out.println("Apakah 3 lebih besar atau sama dengan 3 "
+        //+ (a >= b));
+        //a = 2;
+        //System.out.println("Variabel a diubah : " + a);
+        //System.out.println("Apakah 2 lebih besar atau sama dengan 3 "
+        //+ (a >= b));
+        
+            //OPERATOR SAMA DENGAN DAN TIDAK SAMA DENGAN
+        //int a = 3;
+        //int b = 3;
+        
+        //System.out.println("Variabel a : " + a);
+        //System.out.println("Variabel b : " + b);
+        //System.out.println("Apakah 3 sama dengan 3 " + (a == b)); a = 2;
+        //System.out.println("Apakah 2 sama dengan 3 " + (a == b));
+        //System.out.println("Apakah 2 tidak sama dengan 3 " + (a != b));
+        
+                //OPERATOR BOOLEAN
+            //OPERATOR &&
+        //System.out.println("true and true : " + (true && true));
+        //System.out.println("true and false : " + (true && false));
+        //System.out.println("false and false : " + (false && false));
+        //System.out.println("false and true : " + (false && true));
+        
+            //OPERATOR ||
+        //System.out.println("true or true : " + (true || true));
+        //System.out.println("true or false : " + (true || false));
+        //System.out.println("false or false : " + (false || false));
+        //System.out.println("false or true : " + (false || true));
+    }    
+}
---
+++ Bab7Java.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab.pkg7.java;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Axioo
+ */
+public class Bab7Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            //CARA MEMBUAT ARRAY
+        //CARA 1
+    //String[] nama1;
+        //CARA 2
+    //String nama2[];
+        //CARA 3
+    //String[] nama3= new String[2];
+        
+            //MACAM2 ARRAY
+        //ARRAY 1 DIMENSI
+    //String nama[] = new String[3];
+    //    nama[0] = "Budi";
+    //    nama[1] = "Andi";
+    //    nama[2] = "Dwi";
+
+    //    System.out.println("Index 0 : "+nama[0]);
+    //    System.out.println("Index 1 : "+nama[1]);
+    //    System.out.println("Index 2 : "+nama[2]);
+    
+        //MENGISI ARRAY DARI USER
+    //String[] nama = new
+    //  String[3];
+    //  nama[0] = JOptionPane.showInputDialog(null,"Nama :","index ke-0",JOptionPane.INFORMATION_MESSAGE);
+    //  nama[1] = JOptionPane.showInputDialog(null,"Nama :","index ke-1",JOptionPane.INFORMATION_MESSAGE);
+    //  nama[2] = JOptionPane.showInputDialog(null,"Nama :","index ke-2",JOptionPane.INFORMATION_MESSAGE);
+
+    //  JOptionPane.showMessageDialog(null, nama[0]+", "+nama[1]+", "+nama[2]);
+    
+        //ARRAY MULTI DIMENSI
+    //String umur[][] = new String[2][2];
+    //umur[0][0] = "Budi";
+    //umur[0][1] = "20";
+    //umur[1][0] = "Andi";
+    //umur[1][1] = "45";
+
+    //System.out.println("Nama : "+umur[0][0]+" umur : "+umur[0][1]);
+    //System.out.println("Nama : "+umur[1][0]+" umur : "+umur[1][1]);
+   }
+}
